cdf9701fb0c29b0c9e163d00dbed6c243a305d83
NeyaGupta <106171017+NeyaGupta@users.noreply.github.com>
2024-07-29 02:51:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// const inputBox =document.querySelector('.input-box');
+// const searchBtn =document.querySelector('.searchBtn');
+// const weather_img =document.querySelector('.weather_img');
+// const temperature =document.querySelector('.temperature');
+// const humidity=document.querySelector('.humidity');
+// const description =document.querySelector('.description');
+// const wind_speed =document.querySelector('.wind-speed');
+
+
+// async function checkWeather(city){
+//     const api_key = "8d9057490297d0004ee34bb732d868ce";
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+//     const weather_data = await fetch(`${url}`).then(response => response.json());
+
+//     console.log(weather_data);
+// }
+
+// searchBtn.addEventListener('click', ()=>{
+//     checkWeather(inputBox.value);
+// });
+
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
+
+
+async function checkWeather(city){
+    const api_key = "27e6abfb9568c202e2a03880374e74d2";
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=${api_key}`
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "/images/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/images/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/images/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/images/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/images/snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
